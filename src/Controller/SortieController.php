fe0a63e3b90d934fee2651df682804abb6c60ac7
MarionBeaupied <108260585+MarionBeaupied@users.noreply.github.com>
2022-08-24 12:25:17 +0200

    Update créa sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -25,7 +25,39 @@ class SortieController extends AbstractController
     }
 
     /**
-     * @Route("/sortie/creation", name="app_sortie_creation")
+     * @Route("/sortie/afficher", name="app_afficher_sortie")
+     */
+    public function afficherSortie(): Response
+    {
+        return $this->render('sortie/afficherSortie.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+
+    /**
+     * @Route("/sortie/modifier", name="app_modifier_sortie")
+     */
+    public function modifierSortie(): Response
+    {
+        return $this->render('sortie/modifierSortie.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+
+    /**
+     * @Route("/sortie/annuler", name="app_annuler_sortie")
+     */
+    public function annulerSortie(): Response
+    {
+        return $this->render('sortie/annulerSortie.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+
+//CREATION D'UNE SORTIE : fonction creation et publication
+
+    /**
+     * @Route("/sortie/creation/{id}", name="app_sortie_creation")
      */
     public function sortieCreate(ManagerRegistry $doctrine,$id = -1, Request $request): Response
     {
@@ -63,7 +95,7 @@ class SortieController extends AbstractController
         ]);
     }
     /**
-     * @Route("/sortie/creation", name="app_sortie_creation")
+     * @Route("/sortie/creation", name="app_sortie_publish")
      */
     public function sortiePublish(ManagerRegistry $doctrine,$id = -1, Request $request): Response
     {
@@ -100,4 +132,59 @@ class SortieController extends AbstractController
             "createForm" => $createForm->createView()
         ]);
     }
+
+//MODIFICATION D'UNE SORTIE : fonction update, publication, supprimer
+
+    /**
+     * @Route("/sortie/modifier/{id}", name="app_sortie_modification")
+     */
+    public function sortieModifier(ManagerRegistry $doctrine,$id = -1, Request $request): Response
+    {
+
+        $sortie = new Sortie();
+
+        if ($id > 0 ){
+            $repoSortie = $doctrine->getRepository(Sortie::class);
+
+            $sortie = $repoSortie->find($id);
+        }
+
+        $updateForm = $this->createForm(CreerSortieType::class, $sortie);
+
+        $updateForm->handleRequest($request);
+
+        if ($updateForm->isSubmitted() && $updateForm->isValid()){
+            if ($request->get('save')->isClicked){
+                $sortie->setEtat('En création');
+            }
+            $sortieToSave = $updateForm->getData();
+
+            $sortieToSave->setDateHeureDebut(new \Datetime());
+            $em = $doctrine->getManager();
+            $em->persist($sortieToSave);
+            $em->flush();
+
+            return $this->redirectToRoute("app_home", [
+                "id" => $sortieToSave->getId()
+            ]);
+        }
+
+        return $this->render('sortie/index.html.twig', [
+            "updateForm" => $updateForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/sortie/modifier", name="app_sortie_supprimer")
+     */
+
+    public function deleteSortie(ManagerRegistry $doctrine){
+
+        $sortie = new Sortie();
+
+        $em = $doctrine->getEntityManager();
+        $em->remove($sortie);
+        $em->flush();
+    }
+
 }
