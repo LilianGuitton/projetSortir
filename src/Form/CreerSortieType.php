db8cdec1c0232eb92ec032911a6355efc3b4d344
MarionBeaupied <108260585+MarionBeaupied@users.noreply.github.com>
2022-08-25 17:21:21 +0200

    Update créa sortie

--- src/Form/CreerSortieType.php
+++ src/Form/CreerSortieType.php
@@ -4,10 +4,14 @@ namespace App\Form;
 
 use App\Entity\Sortie;
 use App\Entity\Lieu;
+use phpDocumentor\Reflection\Type;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,12 +22,13 @@ class CreerSortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
+            ->add('nom', TextType::class)
             ->add('dateHeureDebut', DateTimeType::class, [
             ])
-            ->add('duree')
+            ->add('duree', IntegerType::class)
             ->add('dateLimiteInscription', DateTimeType::class)
-            ->add('nbInscriptionMax')
+            ->add('nbInscriptionMax', IntegerType::class)
+            ->add('campus')
             ->add('lieu')
         ;
     }
