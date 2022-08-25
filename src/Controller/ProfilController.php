6a33ddd7b4fdd98cb742ff95363fdf3c49b4b631
mkallouche2022 <mehdy.kallouche2022@campus-eni.fr>
2022-08-25 16:13:54 +0200

    Registration Form mk

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Form\MonProfilType;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,16 +19,27 @@ class ProfilController extends AbstractController
     /**
      * @Route("/profil", name="app_profil")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
 
         $monProfil = $this->getUser();
 
-        
-
         $monProfilForm = $this->createForm(MonProfilType::class, $monProfil);
+
+        if ($monProfilForm->isSubmitted() && $monProfilForm->isValid()){
+
+            $entityManager->persist($monProfil);
+            $entityManager->flush();
+
+            return $this->redirectToRoute("app_home");
+        }
+
         return $this->render('profil/index.html.twig', [
             'MonProfilForm' => $monProfilForm->createView(),
+
+
         ]);
     }
+
+
 }
