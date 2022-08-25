867a9c2a82ba4dd95ed8af69764761af6cc557e9
LilianGuitton <lilian.guitton2022@campus-eni.fr>
2022-08-25 10:55:41 +0200

    Affichage et changement d'état dynamique des sorties

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Campus;
+use App\Entity\Etat;
 use App\Entity\Sortie;
 use App\Form\FiltreType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -38,6 +39,26 @@ class HomeController extends AbstractController
         $repoSortie = $entityManager->getRepository(Sortie::class);
 
         $sortieList = $repoSortie->findAll();
+
+        $now = new \DateTime('now');
+
+        foreach ($sortieList as $sortie){
+            if ($sortie->getEtat()->getLibelle() != "En création"){
+                if ($sortie->getDateLimiteInscription() < $now){
+                    if ($sortie->getDateHeureDebut() > $now){
+                        $sortie->setEtat($entityManager->getRepository(Etat::class)->find(4));
+                    } elseif (date_add($sortie->getDateHeureDebut(), \DateInterval::createFromDateString("+".$sortie->getDuree()." minutes")) > $now){
+                        $sortie->setEtat($entityManager->getRepository(Etat::class)->find(3));
+                    } elseif (date_add($sortie->getDateHeureDebut(), \DateInterval::createFromDateString("+".$sortie->getDuree()." minutes")) < $now and date_add($sortie->getDateHeureDebut(), \DateInterval::createFromDateString("+1 month")) > $now){
+                        $sortie->setEtat($entityManager->getRepository(Etat::class)->find(5));
+                    } else {
+                        $sortie->setEtat($entityManager->getRepository(Etat::class)->find(6));
+                    }
+                    $entityManager->persist($sortie);
+                    $entityManager->flush();
+                }
+            }
+        }
 
         return $this->render('home/index.html.twig', ["sortieList" => $sortieList, "filtre"=>$filtre,
             "filterForm" => $this->createForm(FiltreType::class)->createView()
