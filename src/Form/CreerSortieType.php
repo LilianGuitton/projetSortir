2fcd818e86413d4fa720c85e27c1fc2b324f1316
MarionBeaupied <108260585+MarionBeaupied@users.noreply.github.com>
2022-08-29 17:31:56 +0200

    update css

--- src/Form/CreerSortieType.php
+++ src/Form/CreerSortieType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -22,14 +23,37 @@ class CreerSortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom', TextType::class)
-            ->add('dateHeureDebut', DateTimeType::class, ['widget'=>'single_text'
+            ->add('nom', TextType::class, [
+                "attr" => [
+                    'class' => "form-control",
+                    'style' => "border-radius: 50px"
+                ]
             ])
-            ->add('duree', IntegerType::class)
-            ->add('dateLimiteInscription', DateTimeType::class, ['widget'=>'single_text'
+            ->add('dateHeureDebut', DateTimeType::class, ['widget'=>'single_text', "attr" => [
+                'style' => "border-radius: 20px"
+            ]])
+            ->add('duree', IntegerType::class,[
+                "attr" => [
+                    'class' => "form-control",
+                    'style' => "border-radius: 50px"
+                ]
             ])
-            ->add('nbInscriptionMax', IntegerType::class)
-            ->add('description')
+            ->add('dateLimiteInscription', DateTimeType::class, ['widget'=>'single_text',
+                 "attr" => [
+                    'style' => "border-radius: 20px"
+                ]
+            ])
+            ->add('nbInscriptionMax', IntegerType::class,[
+                "attr" => [
+                    'class' => "form-control",
+                    'style' => "border-radius: 50px"
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ] )
             ->add('lieu')
         ;
     }
