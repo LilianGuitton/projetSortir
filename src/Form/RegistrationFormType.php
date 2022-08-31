78f8c3d256005debcc5a5eb46cc8e5d9a8bfba88
mkallouche2022 <mehdy.kallouche2022@campus-eni.fr>
2022-08-31 15:12:30 +0200

    Registration Form mk

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Participant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,17 +21,37 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
-            ->add('telephone')
-            ->add('estRattacherA')
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo',
+                'attr'=>  ["class" => "form-control form-theme"]
+            ])
+            ->add('nom',TextType::class, [
+                'label' => 'nom',
+                'attr'=>  ["class" => "form-control form-theme"]
+            ])
+            ->add('prenom',TextType::class, [
+                'label' => 'Prenom',
+                'attr'=>  ["class" => "form-control form-theme"]
+            ])
+            ->add('email',EmailType::class, [
+                'label' => 'email',
+                'attr'=>  ["class" => "form-control form-theme"]
+            ])
+            ->add('telephone',TextType::class, [
+                'label' => 'Téléphone',
+                'attr'=>  ["class" => "form-control form-theme"]
+            ])
+            ->add('estRattacherA',EntityType::class, [
+                "class" => Campus::class,
+                "attr" => ["class" => "form-control form-theme"
+                ]
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password','class' => 'form-control form-theme' ],
+
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
